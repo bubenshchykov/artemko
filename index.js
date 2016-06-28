537b645760fc580806144f347d5b90de6169e59a
Artem Kuzko <akuzko@corevalue.net>
2016-06-28 09:33:32 +0300

    kata 2.0 (refacor by artemko)

--- index.js
+++ index.js
@@ -3,9 +3,6 @@ function add(expression) {
 		return 0;
 	}
 	var parts = expression.split(',');
-	if (parts.length === 1) {
-		return Number.parseFloat(parts[0]);
-	}
 
 	var res = parts.reduce(function (accum, curr) {
 		return accum += +curr;
